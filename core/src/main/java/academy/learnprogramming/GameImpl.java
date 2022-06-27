ff6f773addcb42efa120b396423de231210062d2
Johnny Delgado <johnny.m.delgado12@gmail.com>
2022-06-27 17:02:14 -0500

    removed number generator setter and added autowired annotation

--- core/src/main/java/academy/learnprogramming/GameImpl.java
+++ core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -2,8 +2,10 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 
 public class GameImpl implements Game{
@@ -11,6 +13,7 @@ public class GameImpl implements Game{
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     // == fields ==
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 10;
     private int number;
@@ -21,7 +24,6 @@ public class GameImpl implements Game{
     private boolean validNumberRange= true;
 
     // == Init ==
-
     @PostConstruct
     @Override
     public void reset() {
@@ -33,11 +35,12 @@ public class GameImpl implements Game{
         log.debug("the number is {}", number);
     }
 
-    //= public methods ==
-    public void setNumberGenerator(NumberGenerator numberGenerator){
-        this.numberGenerator = numberGenerator;
+    @PreDestroy
+    public void preDestroy() {
+        log.info("in Game preDestroy");
     }
 
+    //= public methods ==
     @Override
     public int getNumber() {
         return number;
