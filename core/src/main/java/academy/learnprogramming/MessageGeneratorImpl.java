f8cd97ea6db143b9d4c88e2628ab538287308b00
Johnny Delgado <johnny.m.delgado12@gmail.com>
2022-06-28 16:39:04 -0500

    changed main message and result message

--- core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -25,11 +25,25 @@ public class MessageGeneratorImpl implements MessageGenerator{
     // == Public methods ==
     @Override
     public String getMainMessage() {
-        return "Calling getMainMessage method";
+        return "Number is between " + game.getSmallest() + " and " + game.getBiggest() + " Can you guess it?";
     }
 
     @Override
     public String getResultMessage() {
-        return "Calling getResultMessage method";
+        if(game.isGameWon()){
+            return "You guessed it! The number was " + game.getNumber();
+        } else if (game.isGameLost()){
+            return "You lost. The number was " + game.getNumber();
+        } else if (!game.isValidNumberRange()) {
+            return "Invalid number range";
+        } else if (game.getRemainingGuesses() == guessCount) {
+            return "What is your first guess?";
+        } else {
+            String direction = "Lower";
+            if (game.getGuess() < game.getNumber()) {
+                direction = "Higher";
+            }
+            return direction + "! You have " + game.getRemainingGuesses() + " guesses left";
+        }
     }
 }
