7e0f91198821a506674b5a76ef00c35827af7315
Johnny Delgado <johnny.m.delgado12@gmail.com>
2022-07-09 10:21:20 -0500

    started converting game to spring boot

---
+++ console/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+import academy.learnprogramming.config.GameConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@Slf4j
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("Guess the Number Game");
+
+        SpringApplication.run(Main.class, args);
+    }
+}
